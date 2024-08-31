bf348d0362ec809e0fdf608026adbf00eb0fe801
damian <dg997678@gmail.com>
2024-08-30 21:29:47 -0500

    add good route

--- src/components/pages/private/createProperty/components/formProperty.tsx
+++ src/components/pages/private/createProperty/components/formProperty.tsx
@@ -43,7 +43,7 @@ const FormProperty: React.FC = () => {
 
     try {
       const response = await fetch(
-        "http://localhost:5000/api/properties/create",
+        "http://localhost:5000/api/properties",
         {
           method: "POST",
           body: formData, // No es necesario especificar Content-Type para FormData
